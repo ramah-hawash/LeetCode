e84bf9a9860585ba7c5968a00364b134cd07c704
Ramah Hawash <153456733+ramah-hawash@users.noreply.github.com>
2024-10-31 23:43:35 +0200

    Time: 0 ms (100.00%) | Memory: 14.4 MB (20.54%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,14 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        unordered_map<int, int> numMap; // لإنشاء خريطة لتخزين العناصر
+        vector<int> result;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i]; // القيمة المفقودة التي تحتاجها
+            // التحقق مما إذا كانت القيمة موجودة في الخريطة
+            if (numMap.find(complement) != numMap.end()) {
+                result.push_back(numMap[complement]); // إضافة الفهرس
+                result.push_back(i); // إضافة فهرس العنصر الحالي
+                return result; // إرجاع النتيجة
             }
+            // إضافة العنصر الحالي إلى الخريطة
+            numMap[nums[i]] = i;
         }
-        return {}; // No solution found
+        
+        return result; // إرجاع النتيجة الفارغة في حالة عدم وجود حل
     }
 };
