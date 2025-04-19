d3c6e64cb064416d06b47f4e75ab1bd3387d5ea7
Ramah Hawash <153456733+ramah-hawash@users.noreply.github.com>
2025-04-19 15:35:50 +0300

    Time: 3 ms (62.67%) | Memory: 11.9 MB (52.70%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-            unordered_map<char, int> numCount;
-            for(int i=0;i<magazine.length();i++){
-                        numCount[magazine[i]]++;
-
-            }
-            for(int i=0;i<ransomNote.length();i++){
-                if(numCount[ransomNote[i]])numCount[ransomNote[i]]--;
-                else return false;
-            }
-            return true;
-
-
-
-
+        unordered_map<char, int> freq;
+        for (char c : magazine) freq[c]++;
+        for (char c : ransomNote) {
+            if (--freq[c] < 0) return false;
+        }
+        return true;
     }
 };
