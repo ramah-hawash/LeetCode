1ef767be8841d435cd8e83b7744b918ecc857ea5
Ramah Hawash <153456733+ramah-hawash@users.noreply.github.com>
2025-04-19 16:03:26 +0300

    Time: 0 ms (100.00%) | Memory: 30.1 MB (98.34%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,21 +1,24 @@
 class Solution {
-    #include <algorithm> // تحتوي على دالة sort
-
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector <int> dou;
-        int j=nums.size()-1;
-        int k=0;
-        int temp=0;
+        int n = nums.size();
+        vector<int> res(n);
+        int left = 0, right = n - 1;
+        int pos = n - 1;
 
-        for(int i=0;i<nums.size();i++){
-            dou.push_back(nums[i] * nums[i]);
+        while (left <= right) {
+            int leftSq = nums[left] * nums[left];
+            int rightSq = nums[right] * nums[right];
 
+            if (leftSq > rightSq) {
+                res[pos--] = leftSq;
+                left++;
+            } else {
+                res[pos--] = rightSq;
+                right--;
+            }
+        }
 
-        }//
-            sort(dou.begin(), dou.end()); // فرز تصاعدي (من الصغير للكبير)
-
-       
-        return dou;
+        return res;
     }
 };
