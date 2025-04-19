6e1ec1a87cba50622b0bc4b2c8d6740051faf562
Ramah Hawash <153456733+ramah-hawash@users.noreply.github.com>
2025-04-19 07:32:00 +0300

    Time: 55 ms (19.08%) | Memory: 90.8 MB (5.72%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, int> students;
-        for(int i=0;i<nums.size();i++){
-            if(students[nums[i]])students[nums[i]]++;
-            else students[nums[i]]=1;
-
-        }//
- for (auto pair : students) {
-    if (pair.second >=2) return true;
-    
-}
-return false;
+        unordered_set<int> seen;
+    for (int n : nums) {
+        if (seen.count(n)) return true;
+        seen.insert(n);
+    }
+    return false;
     
 };
 };
