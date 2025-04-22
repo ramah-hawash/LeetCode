9f51a1960989c4540c8f4ff803f00cb9ea1e8dae
Ramah Hawash <153456733+ramah-hawash@users.noreply.github.com>
2025-04-22 17:37:36 +0300

    Time: 12 ms (39.24%) | Memory: 11.7 MB (79.97%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,13 +9,16 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_set<ListNode*> s;
-        while(head!=nullptr){
-            if(s.count(head))return true;
-            s.insert(head);
-            head=head->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) return true; // التقيوا -> يعني في حلقة
         }
-        return false;
-        
+
+        return false; // وصل للنهاية -> ما في حلقة
     }
 };
